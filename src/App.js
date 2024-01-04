a71b50fc1b6c44b553d5b8db68db06b8cf2d5c1b
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-01-03 17:04:19 -0800

    Final changes before Vite

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import rotatingImage from './Assets/blank-clock-face.png';
 import "./App.css";
+import './Style.scss';
 
 function App() {
   return (
